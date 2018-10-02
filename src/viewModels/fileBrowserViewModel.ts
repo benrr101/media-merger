b74b34cb5ee6fe4054bead211aa1290480a93847
Benjamin Russell <benrr101@outlook.com>
2018-10-02 19:33:48 -0400

    Some sexy changes to make action icons look good But I managed to break opening multiple file trees...

--- src/viewModels/fileBrowserViewModel.ts
+++ src/viewModels/fileBrowserViewModel.ts
@@ -24,13 +24,30 @@ export default class FileBrowserViewModel {
     }
 
     // EVENT HANDLERS //////////////////////////////////////////////////////
-    public handleAddFolder = async () => {
+    public handleClickAddFolder = async () => {
         try {
-            // Get a directory to read files from it
+            // Get a directory
             const folderToAdd = await this._electronApi.getDirectoryPath();
+            if (!folderToAdd) {
+                return;
+            }
+
+            // Generate the root of the tree and kick off recursive initialization
             const folderViewModel = new FileTreeFolderViewModel(this._fileManager, folderToAdd);
             this.fileTrees.push(folderViewModel);
             folderViewModel.init();
+        } catch (e) {
+            // TODO: Display error
+            console.error(e);
+        }
+    }
+
+    public handleClickCloseFolder = async (folderToClose: FileTreeFolderViewModel) => {
+        try {
+            // @TODO: If there are pending changes, ask the user if they want to close
+
+            // Close the folder
+            this.fileTrees.remove(folderToClose);
         } catch (e) {
             // TODO: Display error
             console.error(e);
